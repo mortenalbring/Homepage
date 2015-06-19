28df3c515c854449518237603027685648b6604f
Morten Albring <mortenalbring@gmail.com>
2015-06-19 12:37:41 +0100

    Added bootstrap

--- Homepage/App_Start/BundleConfig.cs
+++ Homepage/App_Start/BundleConfig.cs
@@ -23,7 +23,12 @@ namespace Homepage
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));            
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css")
+                .Include("~/Content/bootstrap.css", "~/Content/bootstrap-responsive.css")
+                );
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",

dbe7b86a793c925bb7256e0e7b3430e0f2b4a235
Morten Albring <electrika.ma@gmail.com>
2015-06-26 15:49:35 +0100

    Added charts and styling

--- Homepage/App_Start/BundleConfig.cs
+++ Homepage/App_Start/BundleConfig.cs
@@ -24,10 +24,12 @@ namespace Homepage
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/d3").Include("~/Scripts/d3/d3.js"));
             
 
             bundles.Add(new StyleBundle("~/Content/css")
-                .Include("~/Content/mortenalbring.css","~/Content/bootstrap.css", "~/Content/bootstrap-responsive.css","~/Content/css/font-awesome.css")
+                .Include("~/Content/mortenalbring.css", "~/Content/mortenalbring-charts.css", "~/Content/bootstrap.css", "~/Content/bootstrap-responsive.css", "~/Content/css/font-awesome.css")
                 );
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(

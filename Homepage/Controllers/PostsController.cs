c513849efd8ca972c126f2017a3d7bf95e71fb53
Morten Albring <mortenalbring@gmail.com>
2015-07-07 13:42:07 +0100

    Added post controller and started work on computations and irreversibility post

---
+++ Homepage/Controllers/PostsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homepage.Controllers
+{
+    public class PostsController : Controller
+    {
+        //
+        // GET: /Posts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ComputationsIrreversibility()
+        {
+            return View();
+        }
+
+    }
+}

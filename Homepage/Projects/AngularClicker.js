a82976c9d753d9c77a93aff0c04c9fde1cf80969
Morten Albring <electrika.ma@gmail.com>
2016-01-05 12:22:46 +0000

    Added autoattack

--- Homepage/Projects/AngularClicker.js
+++ Homepage/Projects/AngularClicker.js
@@ -1,10 +1,11 @@
 var angularClicker = angular.module("AngularClicker",[]);
 
-angularClicker.controller("HomeController", function ($scope) {
+angularClicker.controller("HomeController", function ($interval,$scope) {
     $scope.Player = {
         Name: "",
         XP: 0,
-        Gold: 0
+        Gold: 100,
+        AutoAttack: 0
     }
 
     $scope.Enemies = [];
@@ -17,13 +18,18 @@ angularClicker.controller("HomeController", function ($scope) {
 
     }
 
-
-    $scope.reduceEnemyHealth = function (ID) {
+    $scope.increaseAutoAttack = function() {
+        if ($scope.Player.Gold >= 10) {
+            $scope.Player.AutoAttack = $scope.Player.AutoAttack + 1;
+            $scope.Player.Gold = $scope.Player.Gold - 10;
+        }
+    }
+    $scope.reduceEnemyHealth = function (ID,damage) {
         var enemy = $scope.Enemies.filter(function(e) {
             return e.ID == ID;
         });
         if (enemy.length == 1) {
-            enemy[0].Health = enemy[0].Health - 1;
+            enemy[0].Health = enemy[0].Health - damage;
 
             if (enemy[0].Health <= 0) {
                 var indx = $scope.Enemies.indexOf(enemy[0]);
@@ -33,6 +39,16 @@ angularClicker.controller("HomeController", function ($scope) {
             }
         }        
     }
+    $interval(function() {
+        if ($scope.Player.AutoAttack > 0) {
+
+            for (var i = 0; i < $scope.Enemies.length; i++) {
+                $scope.reduceEnemyHealth($scope.Enemies[i].ID, $scope.Player.AutoAttack);
+            }
+
+        }
+
+    }, 1000);
 
     $scope.generateEnemies();
     $scope.generateEnemies();

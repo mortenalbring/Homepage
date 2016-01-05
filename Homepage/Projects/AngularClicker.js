dcbda5deb3070395b0810df5e3940b60cf63f6f1
Morten Albring <mortenalbring@gmail.com>
2016-01-05 15:50:11 +0000

    Added graveyard and upgrade abilities

--- Homepage/Projects/AngularClicker.js
+++ Homepage/Projects/AngularClicker.js
@@ -27,7 +27,7 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
     $scope.Player = {
         Name: "",
         XP: 0,
-        Gold: 1,
+        Gold: 10,
         AttackDamage: 1,
         AutoAttackDamage: 0
     }
@@ -40,6 +40,7 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
     ]
 
     $scope.Enemies = [];
+    $scope.Graveyard = [];
 
     $scope.MessageLog = [];
 
@@ -49,17 +50,106 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
         Title: "Attack Boost",
         Cost: 5,
         RunFunction: function () {
-            $scope.increaseAttack(5, 1);
+            $scope.buyAttackBoost(this);            
         }
     },
     {
-        Title: "Auto Turret",
+        Title: "Buy Auto Turret",
         Cost: 10,
         RunFunction: function () {
-            $scope.increaseAutoAttack(10, 1);
+            $scope.buyAutoTurret(this);
+        }
+    },
+    {
+        Title: "Bury dead",
+        Cost: 1,
+        RunFunction: function () {
+            $scope.buryDeadEnemies(this.Cost);
+        }
+    },
+    {
+        Title: "Visit graveyard",
+        Cost: 0,
+        RunFunction: function () {
+            $scope.visitGraveyard();
+        }
+    },
+
+    ];
+    $scope.buyAttackBoost = function (element) {
+        if (!$scope.subtractCost(element.Cost)) {
+            return;
+        }
+        $scope.increaseAttack(1);
+
+        element.Cost = element.Cost + 1;
+    }
+
+    $scope.upgradeAutoTurret = function(element) {
+        if (!$scope.subtractCost(element.Cost)) {
+            return;
+        }
+        $scope.increaseAutoAttack(1);
+        element.Cost = element.Cost + 2;
+
+
+    }
+    $scope.buyAutoTurret = function (element) {
+        if (!$scope.subtractCost(element.Cost)) {
+            return;
         }
 
-    }];
+        $scope.Player.AutoAttackDamage = 2;
+
+        var indx = $scope.Shop.indexOf(element);        
+        $scope.Shop.splice(indx, 1);
+        var upgrade = {
+            Title: "Upgrade Auto Turret",
+            Cost: 10,
+            RunFunction: function () {
+                $scope.upgradeAutoTurret(this);
+            }
+        };
+        $scope.Shop.push(upgrade);
+    }
+
+
+    $scope.visitGraveyard = function () {
+        if ($scope.Graveyard.length == 0) {
+            $scope.MessageLog.push("There is nothing here but an empty meadow");
+        } else {
+            $scope.MessageLog.push("You solemnly mourn the " + $scope.Graveyard.length + " graves in the graveyard");
+        }
+    }
+
+    $scope.subtractCost = function (cost) {
+        if ($scope.Player.Gold >= cost) {
+            $scope.Player.Gold = $scope.Player.Gold - cost;
+            return true;
+        }
+        return false;
+
+
+    }
+
+    $scope.buryDeadEnemies = function (cost) {
+        if (!$scope.subtractCost(cost)) {
+            return;
+        }
+
+        var deadEnemies = $scope.Enemies.filter(function (e) {
+            return e.Alive != true;
+        });
+
+        $scope.MessageLog.push("You bury " + deadEnemies.length + " dead enemies");
+
+        for (var i = 0; i < deadEnemies.length; i++) {
+            var indx = $scope.Enemies.indexOf(deadEnemies[i]);
+            $scope.Enemies.splice(indx, 1);
+            $scope.Graveyard.push(deadEnemies[i]);
+        }
+
+    }
 
     $scope.generateEnemies = function (number) {
 
@@ -67,7 +157,7 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
 
         for (var i = 0; i < number; i++) {
 
-            var enemyID = $scope.Enemies.length + 1;
+            var enemyID = guid();
 
             var enemy = {
                 ID: enemyID,
@@ -79,24 +169,18 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
         }
     }
 
-    $scope.increaseAttack = function (cost, val) {
-        if ($scope.Player.Gold >= cost) {
-            $scope.Player.AttackDamage = $scope.Player.AttackDamage + val;
-            $scope.Player.Gold = $scope.Player.Gold - cost;
-        }
+    $scope.increaseAttack = function (val) {
+        $scope.Player.AttackDamage = $scope.Player.AttackDamage + val;
     }
 
-    $scope.increaseAutoAttack = function (cost, val) {
-        if ($scope.Player.Gold >= cost) {
-            $scope.Player.AutoAttackDamage = $scope.Player.AutoAttackDamage + val;
-            $scope.Player.Gold = $scope.Player.Gold - cost;
-        }
+    $scope.increaseAutoAttack = function (val) {    
+        $scope.Player.AutoAttackDamage = $scope.Player.AutoAttackDamage + val;        
     }
     $scope.reduceEnemyHealth = function (ID, damage) {
         var enemy = $scope.Enemies.filter(function (e) {
             return e.ID == ID;
         });
-        
+
         if (enemy.length == 1) {
             if (enemy[0].Health <= 0) {
                 $scope.MessageLog.push(enemy[0].Name + " is already quite dead");
@@ -147,5 +231,16 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
         }
 
     }, 1000);
+
+
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+              .toString(16)
+              .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+          s4() + '-' + s4() + s4() + s4();
+    }
 
 });

05a29e80ad1959e22d5df45aae2f1c3f66fbab6a
Morten Albring <electrika.ma@gmail.com>
2016-01-05 12:13:42 +0000

    Made enemies an array

--- Homepage/Projects/AngularClicker.js
+++ Homepage/Projects/AngularClicker.js
@@ -1,11 +1,43 @@
 var angularClicker = angular.module("AngularClicker",[]);
 
-angularClicker.controller("HomeController", function($scope) {
-    $scope.test = "Moop";
-    $scope.EnemyHealth = 10;
-
-    $scope.reduceEnemyHealth = function() {
-        $scope.EnemyHealth = $scope.EnemyHealth - 1;
+angularClicker.controller("HomeController", function ($scope) {
+    $scope.Player = {
+        Name: "",
+        XP: 0,
+        Gold: 0
     }
+
+    $scope.Enemies = [];
+
+    $scope.generateEnemies = function() {
+        var enemyID = $scope.Enemies.length + 1;
+        
+        var enemy = { ID: enemyID, Health: 10 }
+        $scope.Enemies.push(enemy);
+
+    }
+
+
+    $scope.reduceEnemyHealth = function (ID) {
+        var enemy = $scope.Enemies.filter(function(e) {
+            return e.ID == ID;
+        });
+        if (enemy.length == 1) {
+            enemy[0].Health = enemy[0].Health - 1;
+
+            if (enemy[0].Health <= 0) {
+                var indx = $scope.Enemies.indexOf(enemy[0]);
+                $scope.Enemies.splice(indx, 1);
+                $scope.Player.XP = $scope.Player.XP + 1;
+                $scope.Player.Gold = $scope.Player.Gold + 10;
+            }
+        }        
+    }
+
+    $scope.generateEnemies();
+    $scope.generateEnemies();
+    $scope.generateEnemies();
+    $scope.generateEnemies();
+    $scope.generateEnemies();
 
 });

80492a8823c8dbdb33407927bb736036ace0b7e9
Morten Albring <mortenalbring@gmail.com>
2016-01-05 11:49:38 +0000

    Added single individual enemy

--- Homepage/Projects/AngularClicker.js
+++ Homepage/Projects/AngularClicker.js
@@ -2,5 +2,10 @@ var angularClicker = angular.module("AngularClicker",[]);
 
 angularClicker.controller("HomeController", function($scope) {
     $scope.test = "Moop";
+    $scope.EnemyHealth = 10;
+
+    $scope.reduceEnemyHealth = function() {
+        $scope.EnemyHealth = $scope.EnemyHealth - 1;
+    }
 
 });

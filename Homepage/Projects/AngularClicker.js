ad26407345b70ea2f1e4080ac813970d0c457266
Morten Albring <mortenalbring@gmail.com>
2016-01-05 14:51:13 +0000

    Added directive for mouse and dead mouse

--- Homepage/Projects/AngularClicker.js
+++ Homepage/Projects/AngularClicker.js
@@ -1,5 +1,28 @@
 var angularClicker = angular.module("AngularClicker", []);
 
+angularClicker.directive('enemyMouse', function () {
+    return {
+        template: '<pre>' +
+            '   )' + '\n' +
+            '   (' + '\n' +
+            ' ()-()' + '\n' +
+            '_(o o)_  ' + '\n' +
+            ' /&bsol;o/&bsol; ' + '\n' +
+        '    </pre>'
+    }
+});
+angularClicker.directive('deadMouse', function () {
+    return {
+        template: '<pre>' +
+            '   )' + '\n' +
+            '   (' + '\n' +
+            ' ()-()' + '\n' +
+            '_(x x)_  ' + '\n' +
+            ' /&bsol;o/&bsol; ' + '\n' +
+        '    </pre>'
+    }
+});
+
 angularClicker.controller("HomeController", function ($interval, $scope) {
     $scope.Player = {
         Name: "",
@@ -8,7 +31,14 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
         AttackDamage: 1,
         AutoAttackDamage: 0
     }
-    
+
+    $scope.EnemyTypes = [
+        {
+            Type: "Mouse",
+            Image: ""
+        }
+    ]
+
     $scope.Enemies = [];
 
     $scope.MessageLog = [];
@@ -16,17 +46,17 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
 
     $scope.Shop = [
     {
-        Title:"Attack Boost",
+        Title: "Attack Boost",
         Cost: 5,
-        RunFunction: function() {
-            $scope.increaseAttack(5,1);
+        RunFunction: function () {
+            $scope.increaseAttack(5, 1);
         }
     },
-    {        
+    {
         Title: "Auto Turret",
         Cost: 10,
-        RunFunction: function() {
-            $scope.increaseAutoAttack(10,1);
+        RunFunction: function () {
+            $scope.increaseAutoAttack(10, 1);
         }
 
     }];
@@ -49,14 +79,14 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
         }
     }
 
-    $scope.increaseAttack = function(cost,val) {
+    $scope.increaseAttack = function (cost, val) {
         if ($scope.Player.Gold >= cost) {
             $scope.Player.AttackDamage = $scope.Player.AttackDamage + val;
             $scope.Player.Gold = $scope.Player.Gold - cost;
         }
     }
 
-    $scope.increaseAutoAttack = function (cost,val) {
+    $scope.increaseAutoAttack = function (cost, val) {
         if ($scope.Player.Gold >= cost) {
             $scope.Player.AutoAttackDamage = $scope.Player.AutoAttackDamage + val;
             $scope.Player.Gold = $scope.Player.Gold - cost;
@@ -66,7 +96,12 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
         var enemy = $scope.Enemies.filter(function (e) {
             return e.ID == ID;
         });
+        
         if (enemy.length == 1) {
+            if (enemy[0].Health <= 0) {
+                $scope.MessageLog.push(enemy[0].Name + " is already quite dead");
+                return;
+            }
             enemy[0].Health = enemy[0].Health - damage;
             $scope.MessageLog.push("You attack " + enemy[0].Name + " for " + damage + " pts of damage");
 
@@ -97,15 +132,15 @@ angularClicker.controller("HomeController", function ($interval, $scope) {
                 if ($scope.Enemies[i].Alive) {
                     $scope.reduceEnemyHealth($scope.Enemies[i].ID, $scope.Player.AutoAttackDamage);
                 }
-                
+
             }
 
         }
         var rand = Math.random();
 
-        var aliveEnemies = $scope.Enemies.filter(function (e) { return e.Alive == true;})
+        var aliveEnemies = $scope.Enemies.filter(function (e) { return e.Alive == true; })
 
-        if ((rand > 0.5) && (aliveEnemies.length < 10)) {
+        if ((rand > 0.1) && (aliveEnemies.length < 20)) {
             var message = "A new enemy has appeared!";
             $scope.MessageLog.push(message);
             $scope.generateEnemies(1);

80bf4161727ab152228715c92a75de9070541380
Morten <mortenalbring@gmail.com>
2016-01-06 14:21:16 +0000

    Moved more methods over to services

--- Homepage/Projects/AngularClicker/AngularClicker.js
+++ Homepage/Projects/AngularClicker/AngularClicker.js
@@ -1,7 +1,7 @@
 var angularClicker = angular.module("AngularClicker", []);
 
 angularClicker.service('MessageService', AngularClickerMessageService);
-angularClicker.service('EnemyService', AngularClickerEnemyService);
+angularClicker.service('EnemyService', ['MessageService', AngularClickerEnemyService]);
 angularClicker.service('PlayerService', AngularClickerPlayerService);
 angularClicker.service('ShopService', ['MessageService', 'PlayerService', 'EnemyService', AngularClickerShopService]);
 
@@ -75,87 +75,28 @@ angularClicker.directive('playerFace', function () {
 })
 
 angularClicker.controller("HomeController", function (MessageService, PlayerService, EnemyService, ShopService, $interval, $scope) {
-    $scope.Player = PlayerService.Player;
-
-    $scope.subtractCost = function (cost) {
-        if ($scope.Player.Gold >= cost) {
-            $scope.Player.Gold = $scope.Player.Gold - cost;
-            return true;
-        }
-        return false;
-
-
-    }
-
-    $scope.EnemyTypes = [
-        {
-            Type: "Mouse",
-            Image: ""
-        }
-    ]
-
+    $scope.Player = PlayerService.Player;    
     $scope.Enemies = EnemyService.Enemies;
-    $scope.Graveyard = [];
-
+    $scope.Graveyard = EnemyService.Graveyard;
     $scope.MessageLog = MessageService.MessageLog;
-
-
-    $scope.Shop = ShopService.Shop;
-
-
-    $scope.generateEnemies = function (number) {
-
-        if (!number) { number = 1 };
-
-        for (var i = 0; i < number; i++) {
-
-            var enemyID = guid();
-
-            var enemy = {
-                ID: enemyID,
-                Name: "Mouse",
-                Alive: true,
-                Health: 10,
-                MaxHealth: 10
-            }
-            EnemyService.Enemies.push(enemy);
-        }
-    }
-
+    $scope.Shop = ShopService.Shop; 
    
     $scope.manualAttack = function (ID) {        
         $scope.Player.Status = 1;
         $scope.reduceEnemyHealth(ID, $scope.Player.AttackDamage);
-
     }
 
     $scope.reduceEnemyHealth = function (ID, damage) {
-        var enemy = $scope.Enemies.filter(function (e) {
-            return e.ID == ID;
-        });
-
-        if (enemy.length == 1) {
-            if (enemy[0].Health <= 0) {
-                $scope.MessageLog.push(enemy[0].Name + " is already quite dead");
-                return;
-            }
-            enemy[0].Health = enemy[0].Health - damage;
-            $scope.MessageLog.push("You attack " + enemy[0].Name + " for " + damage + " pts of damage");
-
-            if (enemy[0].Health <= 0) {
-                enemy[0].Alive = false;
-                $scope.MessageLog.push(enemy[0].Name + " is dead");
-                $scope.Player.XP = $scope.Player.XP + 1;
-                $scope.Player.Gold = $scope.Player.Gold + 10;
-            }
+        var killed = EnemyService.ReduceEnemyHealth(ID, damage);
+        if (killed) {
+            PlayerService.Player.IncreaseXP(10);
+            PlayerService.Player.IncreaseGold(1);
         }
     }
 
 
     $scope.getColour = function (enemy) {
-
         var frac = (enemy.Health / enemy.MaxHealth);
-
         var outcss = "rgba(" + parseInt(frac * 255) + ",0,0,1)";
         return outcss;
     }
@@ -163,14 +104,11 @@ angularClicker.controller("HomeController", function (MessageService, PlayerServ
     $scope.initialise = function () {
         $scope.started = true;
 
-        $scope.generateEnemies(5);
-
-
-
+        EnemyService.GenerateEnemies(5);
     }
+
     $interval(function () {
         $scope.Player.Status = 0;
-
         if (!$scope.started) {
             return;
         }
@@ -180,7 +118,6 @@ angularClicker.controller("HomeController", function (MessageService, PlayerServ
                 if ($scope.Enemies[i].Alive) {
                     $scope.reduceEnemyHealth($scope.Enemies[i].ID, $scope.Player.AutoAttackDamage);
                 }
-
             }
         }
 
@@ -189,8 +126,6 @@ angularClicker.controller("HomeController", function (MessageService, PlayerServ
             if (buried > 0) {
                 MessageService.MessageLog.push("The gravedigger buries " + buried + " dead enemies");
             }
-
-
         }
 
         var rand = Math.random();
@@ -199,21 +134,9 @@ angularClicker.controller("HomeController", function (MessageService, PlayerServ
 
         if ((rand > 0.5) && (aliveEnemies.length < 5)) {
             var message = "A new enemy has appeared!";
-            $scope.MessageLog.push(message);
-            $scope.generateEnemies(1);
+            MessageService.MessageLog.push(message);
+            EnemyService.GenerateEnemies(1);
         }
 
-    }, 1000);
-
-
-    function guid() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-              .toString(16)
-              .substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-          s4() + '-' + s4() + s4() + s4();
-    }
-
+    }, 1000);  
 });

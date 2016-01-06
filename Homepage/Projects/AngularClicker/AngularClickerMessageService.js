4a6559010d58e9e5e9d96e0232e64bf1c5a1d64d
Morten <mortenalbring@gmail.com>
2016-01-06 14:06:27 +0000

    Added EnemyService and MessageService and moved relevant methods across

---
+++ Homepage/Projects/AngularClicker/AngularClickerMessageService.js
@@ -0,0 +1,4 @@
+var AngularClickerMessageService = function() {
+    this.MessageLog = [];
+
+}
--- Homepage/Projects/AngularClicker/AngularClickerShopService.js
+++ Homepage/Projects/AngularClicker/AngularClickerShopService.js
@@ -1,4 +1,6 @@
-var AngularClickerShopService = function (PlayerService) {
+var AngularClickerShopService = function (MessageService, PlayerService, EnemyService) {
+    var self = this;
+
     this.Shop = [
     {
         Title: "Attack Boost",
@@ -11,32 +13,74 @@ var AngularClickerShopService = function (PlayerService) {
         Title: "Buy Auto Turret",
         Cost: 10,
         RunFunction: function () {
-            $scope.buyAutoTurret(this);
+            buyAutoTurret(this);
         }
     },
     {
         Title: "Bury dead",
         Cost: 1,
         RunFunction: function () {
-            $scope.buryDeadEnemies(this.Cost);
+            buryDeadEnemies(this);
         }
     },
     {
         Title: "Hire gravedigger",
         Cost: 20,
         RunFunction: function () {
-            $scope.hireGravedigger(this);
+            hireGraveDigger(this);
         }
     },
     {
         Title: "Visit graveyard",
         Cost: 0,
         RunFunction: function () {
-            $scope.visitGraveyard();
+            visitGraveyard();
         }
     },
 
     ];
+
+    function visitGraveyard() {
+        if (EnemyService.Graveyard.length == 0) {
+            MessageService.MessageLog.push("There is nothing here but an empty meadow");
+        } else {
+            PlayerService.Player.Status = 2;
+            MessageService.MessageLog.push("You solemnly mourn the " + EnemyService.Graveyard.length + " graves in the graveyard");
+        }
+    }
+    
+    function buryDeadEnemies(element) {
+        var deadEnemies = EnemyService.Enemies.filter(function (e) {
+            return e.Alive != true;
+        });
+        if (deadEnemies.length == 0) {
+            return;
+        }
+
+        if (!PlayerService.Player.SubtractCost(element.Cost)) {
+            return;
+        }
+
+        PlayerService.Player.Status = 2;
+
+        var buried = EnemyService.BuryDead();
+
+        MessageService.MessageLog.push("You bury " + buried + " dead enemies");
+        
+        
+        
+    }
+
+    function hireGraveDigger(element) {
+        if (!PlayerService.Player.SubtractCost(element.Cost)) {
+            return;
+        }
+
+        MessageService.MessageLog.push("You hire a kindly old gravedigger to clear out corpses for you");
+        PlayerService.Player.AutoBury = true;
+        var indx = self.Shop.indexOf(element);
+        self.Shop.splice(indx, 1);
+    }
 
     function buyAttackBoost(element) {
         if (!PlayerService.Player.SubtractCost(element.Cost)) {
@@ -46,5 +90,33 @@ var AngularClickerShopService = function (PlayerService) {
 
         element.Cost = element.Cost + 1;
     }
+
+    function buyAutoTurret(element) {
+        if (!PlayerService.Player.SubtractCost(element.Cost)) {
+            return;
+        }
+        PlayerService.Player.IncreaseAutoAttack(1);
+        
+        var indx = self.Shop.indexOf(element);
+        self.Shop.splice(indx, 1);
+        var upgrade = {
+            Title: "Upgrade Auto Turret",
+            Cost: 10,
+            RunFunction: function () {
+                upgradeAutoTurret(this);
+            }
+        };
+        self.Shop.push(upgrade);
+    }
+
+    function upgradeAutoTurret(element) {
+        if (!PlayerService.Player.SubtractCost(element.Cost)) {
+            return;
+        }
+        PlayerService.Player.IncreaseAutoAttack(2);
+        element.Cost = element.Cost + 2;
+
+    }
+
 
 }

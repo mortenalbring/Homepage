37afe4e75319f4cfa8b342b177b43d8cc10a700d
Morten Albring <mortenalbring@gmail.com>
2016-01-06 22:54:05 +0000

    Added models for input and output

--- Homepage/Projects/AngularClicker/AngularClickerASCIIGen.js
+++ Homepage/Projects/AngularClicker/AngularClickerASCIIGen.js
@@ -1,6 +1,34 @@
-var asciiGenerator = angular.module("ASCIIGenerator", []);
+var asciiGenerator = angular.module("ASCIIGenerator", ['ngSanitize']);
 
 asciiGenerator.controller("HomeController", function ($scope) {
 
     $scope.test = "moop";
+    $scope.output = "";
+
+    $scope.generate = function () {
+        
+
+        
+        console.log($scope.output);
+
+        
+        var spl = $scope.input.split("\n");
+        var out = [];
+        for (var i = 0; i < spl.length; i++) {
+            var line = "&#39;" + spl[i] + "&#39;" + "+ '\\n' + ";
+            out.push(line);
+            console.log(line);
+
+        }
+        $scope.output = out.join('\n');
+        console.log($scope.output);
+
+        $scope.output = $scope.input.replace("\\", "&bsol;");
+    }
 });
+
+asciiGenerator.filter('to_trusted', ['$sce', function ($sce) {
+    return function (text) {
+        return $sce.trustAsHtml(text);
+    };
+}]);

aea18f44819df481344bdc16091a16eabc0e3390
Morten <mortenalbring@gmail.com>
2016-01-07 10:55:12 +0000

    Made the generator preserve whitespace and convert some dangerous characters into HTML codes

--- Homepage/Projects/AngularClicker/AngularClickerASCIIGen.js
+++ Homepage/Projects/AngularClicker/AngularClickerASCIIGen.js
@@ -1,34 +1,28 @@
-var asciiGenerator = angular.module("ASCIIGenerator", ['ngSanitize']);
+var asciiGenerator = angular.module("ASCIIGenerator",[]);
 
 asciiGenerator.controller("HomeController", function ($scope) {
-
-    $scope.test = "moop";
     $scope.output = "";
 
     $scope.generate = function () {
+                
         
-
-        
-        console.log($scope.output);
 
         
         var spl = $scope.input.split("\n");
         var out = [];
-        for (var i = 0; i < spl.length; i++) {
-            var line = "&#39;" + spl[i] + "&#39;" + "+ '\\n' + ";
+        out.push("'<pre>' + '\\n' +");
+        for (var i = 0; i < spl.length; i++) {            
+            var safe = spl[i].replace("\\", "&bsol;");
+            console.log(spl[i]);
+
+            var line = "'" + safe + "'" + " + '\\n' + ";
             out.push(line);
-            console.log(line);
-
+            //console.log(line);
         }
+        out.push("</pre>");
         $scope.output = out.join('\n');
-        console.log($scope.output);
+        //console.log($scope.output);
 
-        $scope.output = $scope.input.replace("\\", "&bsol;");
+        //$scope.output = $scope.input.replace("\\", "&bsol;");
     }
 });
-
-asciiGenerator.filter('to_trusted', ['$sce', function ($sce) {
-    return function (text) {
-        return $sce.trustAsHtml(text);
-    };
-}]);

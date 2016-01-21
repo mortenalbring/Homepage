9b704f656dfec01f5a7284ead3eef3258fedcfca
Morten Albring <mortenalbring@gmail.com>
2016-01-21 14:34:55 +0000

    Added new object for GameInfo

---
+++ Homepage/Models/Amenhokit/GameInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homepage.Models.Amenhokit
+{
+    public class GameInfo
+    {
+        public int Lane { get; set; }
+        public int GameNumber { get; set; }
+        public DateTime Date { get; set; }
+    }
+}

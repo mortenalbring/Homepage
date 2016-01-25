bf09291844e58a94e0817431ebe08d20f0dd0122
Morten Albring <mortenalbring@gmail.com>
2016-01-25 13:42:43 +0000

    Added unit tests to test the ParseScoreString method

---
+++ Homepage.Tests/AmenhokitScoreTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Homepage.Models.Amenhokit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homepage.Tests
+{
+    [TestClass]
+    public class AmenhokitScoreTests
+    {
+        [TestMethod]
+        public void TestScoreCalculator1()
+        {
+            var player = new PlayerInfo();
+            player.ScoreString = "MORT 1 2 X 1 / 1 4 3 / 1 2 X 3 X 2 3 2";
+
+            player.ScoreString = "MORT 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1";
+
+            player.ParseScoreString();
+
+            Assert.AreEqual(20, player.Score);
+        }
+        [TestMethod]
+        public void TestScoreCalculator2()
+        {
+            var player = new PlayerInfo();            
+            player.ScoreString = "MORT 9 / 9 / 9 / 9 / 9 / 9 / 9 / 9 / 9 / 9 / 1";
+
+            player.ParseScoreString();
+
+            Assert.AreEqual(182, player.Score);
+        }
+        [TestMethod]
+        public void TestScoreCalculator3()
+        {
+            var player = new PlayerInfo();
+            player.ScoreString = "MORT 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 / 1";
+
+            player.ParseScoreString();
+
+            Assert.AreEqual(11, player.Score);
+        }
+
+        [TestMethod]
+        public void TestScoreCalculator4()
+        {
+            var player = new PlayerInfo();
+            player.ScoreString = "MORT 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 9 / 5";
+
+            player.ParseScoreString();
+
+            Assert.AreEqual(15, player.Score);
+        }
+
+
+        [TestMethod]
+        public void TestScoreCalculator5()
+        {
+            var player = new PlayerInfo();
+            player.ScoreString = "MORT 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 X 1 1";
+
+            player.ParseScoreString();
+
+            Assert.AreEqual(12, player.Score);
+        }
+
+        [TestMethod]
+        public void TestScoreCalculator6()
+        {
+            var player = new PlayerInfo();
+            player.ScoreString = "MORT 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 5 / X";
+
+            player.ParseScoreString();
+
+            Assert.AreEqual(20, player.Score);
+        }
+
+        [TestMethod]
+        public void TestScoreCalculator7()
+        {
+            var player = new PlayerInfo();
+            player.ScoreString = "MORT X X 5 3 9 / 6 2 0 7 8 1 0 0 X X X X";
+
+            player.ParseScoreString();
+
+            Assert.AreEqual(151, player.Score);
+        }
+
+        [TestMethod]
+        public void TestScoreCalculator8()
+        {
+            var player = new PlayerInfo();
+            player.ScoreString = "MORT 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 X X X";
+
+            player.ParseScoreString();        
+
+            Assert.AreEqual(30, player.Score);
+        }
+
+        [TestMethod]
+        public void TestScoreCalculator9()
+        {
+            var player = new PlayerInfo();
+            player.ScoreString = "MORT 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 X X X X";
+
+            player.ParseScoreString();
+
+            Assert.AreEqual(60, player.Score);
+        }
+    }
+}

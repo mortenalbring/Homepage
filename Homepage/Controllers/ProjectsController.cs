d1e90ad6967431806195b76e3f73a2271e14e52e
Morten Albring <mortenalbring@gmail.com>
2016-01-27 16:42:59 +0000

    Updated references

--- Homepage/Controllers/ProjectsController.cs
+++ Homepage/Controllers/ProjectsController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Homepage.Models.Amenhokit;
+using Homepage.Models.Amenhokit.PdfScan;
 using Newtonsoft.Json;
 using WebMatrix.WebData;
 
--- Homepage/Models/DataContext.cs
+++ Homepage/Models/DataContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Homepage.Models.Amenhokit;
+using Homepage.Models.Amenhokit.PdfScan;
 
 namespace Homepage.Models
 {

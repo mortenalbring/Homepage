8d14caf7334d3765bbf94d2c0d18e143a56b6b3d
Morten Albring <mortenalbring@gmail.com>
2016-01-27 16:49:07 +0000

    Added new models for database

---
+++ Homepage/Models/Amenhokit/Database/PlayerScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homepage.Models.Amenhokit.Database
+{
+    public class PlayerScore
+    {
+        public int ID { get; set; }
+        public int Session { get; set; }
+        public int Game { get; set; }
+        public int Score { get; set; }
+        public string Scorestring { get; set; }
+    }
+}

f392c1e7ab15107ea01ebeb27ed61696d11f49d4
Morten Albring <mortenalbring@gmail.com>
2016-02-18 17:14:01 +0000

    Moved directives to separate folder

--- Homepage/Projects/AmenhokitApp/Controllers/DisplayController.js
+++ Homepage/Projects/AmenhokitApp/Controllers/DisplayController.js
@@ -1,34 +1,10 @@
-﻿var DisplayController = function ($routeParams, $scope, DataService, GraphService) { 
+﻿var DisplayController = function ($routeParams, $scope, DataService) { 
     $scope.sessions = DataService.sessions;
     $scope.games = DataService.games;
     $scope.playerscores = DataService.playerscores;
     $scope.players = DataService.players;
     $scope.loading = false;
-
-    $scope.chartData =[
-    {
-        hour: 1, sales : 54
-        },
-        {hour: 2, sales: 66
-    },
-    {
-        hour: 3, sales: 77
-    },
-        {hour : 4, sales: 70},
-        {hour : 5, sales: 60
-    },
-        {hour : 6, sales : 63},
-    {
-        hour: 7, sales: 55},
-        {hour : 8, sales: 47
-        },
-    {
-            hour: 9, sales: 55
-    },
-        {hour : 10, sales: 30
-        }
-        ];
-
+    $scope.chartData = [];
 
 
     var finalCallback = function () {

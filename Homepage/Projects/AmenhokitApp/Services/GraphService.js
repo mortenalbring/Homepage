7c4be4d4f6f4fda24fa67c8453438f4bfa8a29d6
Morten Albring <mortenalbring@gmail.com>
2016-02-18 15:20:33 +0000

    Added d3 and new directive for drawing graph

---
+++ Homepage/Projects/AmenhokitApp/Services/GraphService.js
@@ -0,0 +1,17 @@
+﻿var GraphService = function() {
+    /*
+    this.graph = {
+        width: parseInt(d3.select('#graph-container').style('width'), 10),
+        height: 600,
+        margin: 30,
+        svg: null,
+        force: null,
+        data: {
+            nodes: [],
+
+            edges: []
+        }
+    };
+    */
+
+}

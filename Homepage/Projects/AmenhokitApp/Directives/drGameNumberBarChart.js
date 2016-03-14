d0f3b9a7507a555c60b0801a8628c13dde14685a
Morten Albring <mortenalbring@gmail.com>
2016-03-14 17:12:04 +0000

    Adjusted widths and spacing of graph elements

--- Homepage/Projects/AmenhokitApp/Directives/drGameNumberBarChart.js
+++ Homepage/Projects/AmenhokitApp/Directives/drGameNumberBarChart.js
@@ -1,13 +1,13 @@
 ﻿var drGameNumberBarChart = function ($rootScope, $window, $location) {
     return {
         restrict: "EA",
-        template: "<div class='graph-control-container'>" +
+        template: "<div class='graph-control-container full-height'>" +
            "<ul class='graph-button-container'>" +
            "<li ng-class=\"{'button-active' : sortprop == 'gamenumber'}\" ng-click=sortBy('gamenumber',true)>Sort by game number</li>" +
            "<li ng-class=\"{'button-active' : sortprop == 'average'}\" ng-click=sortBy('average',true)>Sort by score</li>" +
            "<li ng-class=\"{'button-active' : sortprop == 'count'}\" ng-click=sortBy('count',true)>Sort by play count</li>" +
            "</ul>" +
-            "<div class='barchart'>" +
+            "<div class='barchart-gamenumber'>" +
            "</div>" +
            "</div>",
 
@@ -41,8 +41,11 @@
             }
 
             var d3 = $window.d3;
-            var rawSvg = elem.find(".barchart")[0];
+            var rawSvg = elem.find(".barchart-gamenumber")[0];
             var svg = d3.select(rawSvg);
+
+            var elemheight = svg.style('height');
+
 
             var plotData = scope[attrs.chartData];
             var playerAverage = scope[attrs.playerAverage];
@@ -73,23 +76,17 @@
                 var newbar = svg.selectAll("div")
                     .data(plotData)
                     .enter().append("div")
-
-                    .style("background-color", function (d) {
-                        var color = colorscale(d.Average);
+                    .attr("class","gamenumber-bar")
+                    .style("background-color", function(d) {                        
                         return colorscale(d.Average);
-                    })
-                    .attr("class", function (d) {
-                        if (d.Average > playerAverage) {
-                            return "bar goodlane";
-                        }
-                        return "bar badlane";
                     });
+           
 
-                newbar.style("width", "10%")
-                .transition()
-                .duration(1000)
+                newbar.style("height", "10%")
+                    .transition()
+                    .duration(1000)
                     .ease('bounce')
-                .style("width", function (d) { return x(d.Average) + "%"; })
+                    .style("height", function (d) { return x(d.Average) + "%"; });                
 
                 newbar.on("mouseover", function (d) {
                     tooltip.transition()
@@ -115,22 +112,25 @@
                 });
 
                 newbar.append("div")
-                    .attr("class", "lanebar-info")
+                    .attr("class", "gamenumber-info")
                     .text(function (d) {
                         return "Game " + d.GameNumber;
                     });
                 newbar.append("div")
-                    .attr("class", "lanebar-info")
+                    .attr("class", "gamenumber-info")
                     .text(function (d) {
-                        return "Average score: " + d.Average;
+                        return "Average : " + d.Average;
                     });
 
 
+                /*
                 svg.insert("div", ":first-child")
-                .style("width", function () { return x(playerAverage) + "%"; })
+                .style("height", function () { return x(playerAverage) + "%"; })
                     .style("background-color", function () { return colorscale(playerAverage) })
                 .attr("class", "bar playeraverage")
                 .text(function () { return "Player Average : " + playerAverage });
+                */
+
                 var tooltip = svg.append("div")
                  .attr("class", "tooltip")
                  .style("opacity", 0);

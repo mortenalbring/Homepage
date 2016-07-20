af50501f6bfb5956d4248540533ed751a61cde01
Morten Albring <mortenalbring@gmail.com>
2016-07-20 22:27:34 +0100

    Added game loop

--- Homepage/Projects/PennyGame/PennyGame.js
+++ Homepage/Projects/PennyGame/PennyGame.js
@@ -1,27 +1,95 @@
 ﻿var pennyGame = angular.module("PennyGame", []);
 
-pennyGame.controller("HomeController", function ($scope) {
-    $scope.test = "moop";
-
+pennyGame.controller("HomeController", function ($interval, $scope) {
     $scope.results = [];
+
+    $scope.pennyInput = ["T", "T", "H"];
+
+    $scope.input = ["H", "H", "T"];
+
+    
+    
+    $scope.reset = function () {
+        $scope.winner = false;
+        $scope.results.length = 0;
+        $scope.userWins = 0;
+        $scope.pennyWins = 0;
+        $scope.totalHeads = 0;
+        $scope.totalTails = 0;
+        $scope.totalWins = 0;
+        $scope.gameNumber = 0;
+    }
+    $scope.reset();
+
+    var gameLoop;
+
+    $scope.stopLoop = function () {
+
+        if (angular.isDefined(gameLoop)) {
+            $interval.cancel(gameLoop);
+        }
+    }
+
+    $scope.playGames = function (max) {
+        $scope.reset();
+
+        gameLoop = $interval(function () {           
+            $scope.flipCoin(1);
+
+            if ($scope.winner) {
+
+                if ($scope.gameNumber == (max-1)) {
+                    $scope.stopLoop();
+                }
+
+                $scope.gameNumber++;
+                $scope.results.length = 0;
+            }
+        }, 10)
+    }
 
     $scope.flipCoin = function (iterations) {
         for (var i = 0; i < iterations; i++) {
-            var randNumber = Math.random();
 
-            if (randNumber < 0.5) {
+            var x = (Math.floor(Math.random() * 2) == 0);
+            
+            if (x) {
                 $scope.result = "H";
+                $scope.totalHeads++;
             }
             else {
                 $scope.result = "T";
+                $scope.totalTails++;
             }
             $scope.results.push($scope.result);
+
+            checkForWinner();
+
+            if ($scope.winner) {
+                break;
+            }
         }
 
     }
 
-    $scope.headstotal = $scope.results.filter(function (e) {
-        return e == "H"
-    });
+    function checkForWinner() {
+        var inputString = $scope.input.join();
+        var pennyString = $scope.pennyInput.join();
+        var resultsString = $scope.results.join();
+
+        if (resultsString.indexOf(inputString) > -1) {
+            $scope.winner = true;
+            $scope.userWins++;
+            return;
+        }
+        if (resultsString.indexOf(pennyString) > -1) {
+            $scope.winner = true;
+            $scope.pennyWins++;
+            return;
+        }
+
+        $scope.winner = false;
+    }
+
 
 })

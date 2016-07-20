ba4f2709f15f769d9f09c1b4cf23b2546fdfa5be
Morten Albring <mortenalbring@gmail.com>
2016-07-20 21:43:28 +0100

    Added flip coin functionality

--- Homepage/Projects/PennyGame/PennyGame.js
+++ Homepage/Projects/PennyGame/PennyGame.js
@@ -2,4 +2,26 @@
 
 pennyGame.controller("HomeController", function ($scope) {
     $scope.test = "moop";
+
+    $scope.results = [];
+
+    $scope.flipCoin = function (iterations) {
+        for (var i = 0; i < iterations; i++) {
+            var randNumber = Math.random();
+
+            if (randNumber < 0.5) {
+                $scope.result = "H";
+            }
+            else {
+                $scope.result = "T";
+            }
+            $scope.results.push($scope.result);
+        }
+
+    }
+
+    $scope.headstotal = $scope.results.filter(function (e) {
+        return e == "H"
+    });
+
 })

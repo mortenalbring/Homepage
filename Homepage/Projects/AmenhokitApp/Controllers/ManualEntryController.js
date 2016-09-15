f0d2beb16a72900edb374695616a954f924201a5
Morten Albring <mortenalbring@gmail.com>
2016-09-15 13:54:09 +0100

    Added controller and view for manual entry

---
+++ Homepage/Projects/AmenhokitApp/Controllers/ManualEntryController.js
@@ -0,0 +1,8 @@
+﻿var ManualEntryController = function($routeParams, $scope, DataService, BowlingService) {
+    $scope.calculateScore = function(string) {
+        var score = BowlingService.CalculateFrameScores(string);
+
+        $scope.scoreStringCalculated = score;
+
+    }
+}

8636280b76f0f698a411be06ce49a94f6f5823c4
Morten Albring <mortenalbring@gmail.com>
2017-06-20 09:38:59 +0100

    Loading session info

--- Homepage/Projects/NewAmenhokitApp/Controllers/SessionsController.js
+++ Homepage/Projects/NewAmenhokitApp/Controllers/SessionsController.js
@@ -7,9 +7,38 @@
 
         this.dataService = DataService;
 
-        this.dataService.getScoresBySession(sessionId).then(function(result) {
-            var yy = 42;
+        this.players = this.dataService.players;
+        this.sessions = this.dataService.sessions;
+        this.playerScores = this.dataService.playerScores;
 
+        //todo some logic to check for existing data        
+
+        this.session = null;
+        var self = this;
+
+        this.dataService.getScoresBySession(sessionId).then(function(result) {            
+            for (var i = 0; i < result.data.length; i++) {
+                var player = result.data[i].Player;
+                var playerScore = result.data[i].PlayerScore;
+                var session = result.data[i].Session;
+                var game = result.data[i].Game;
+                var sessionDate = result.data[i].DateTime;
+
+                self.dataService.addPlayer(player);
+                self.dataService.addSession(session, sessionDate);
+                self.dataService.addGame(game);
+                self.dataService.addPlayerScore(playerScore);
+                
+            }
+
+            var sessionObj = self.sessions.filter(function(e) {
+                return e.ID == sessionId;
+            });
+            if (sessionObj.length == 0) {
+                console.log("No session found");
+                return;
+            }
+            self.session = sessionObj[0];
         });
 
         var xx = 42;

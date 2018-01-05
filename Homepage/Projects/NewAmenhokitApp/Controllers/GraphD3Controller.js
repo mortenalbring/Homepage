9dcff41a94e3ae3299c005eebd337399b1b1aa61
Morten Albring <mortenalbring@gmail.com>
2018-01-05 13:26:03 +0000

    Refactored

--- Homepage/Projects/NewAmenhokitApp/Controllers/GraphD3Controller.js
+++ Homepage/Projects/NewAmenhokitApp/Controllers/GraphD3Controller.js
@@ -1,4 +1,4 @@
-﻿var GraphD3Controller = function() {
+﻿var GraphD3Controller = function () {
     GraphD3Controller.$inject = [
         "$rootScope", "$scope", "$state", "$window", "$timeout", "DataService", "BowlingService"
     ];
@@ -7,120 +7,138 @@
 
         this.dataService = DataService;
 
-        var svg = d3.select("#graph-container"),
-            margin = { top: 20, right: 80, bottom: 50, left: 50 },
-            width = svg.attr("width") - margin.left - margin.right,
-            height = svg.attr("height") - margin.top - margin.bottom,
-            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        this.svg = d3.select("#graph");
+        this.margin = { top: 20, right: 80, bottom: 50, left: 50 };
 
-        var parseTime = d3.timeParse("%Y%m%d");
+        this.width = this.svg.attr("width") - this.margin.left - this.margin.right;
+        this.height = this.svg.attr("height") - this.margin.top - this.margin.bottom;
+            this.g = this.svg.append("g").attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
-        var x = d3.scaleTime().range([0, width]),
-            y = d3.scaleLinear().range([height, 0]),
-            z = d3.scaleOrdinal(d3.schemeCategory10);
+
+        this.parseTime = d3.timeParse("%Y%m%d");
+
+        this.x = d3.scaleTime().range([0, this.width]);
+        this.y = d3.scaleLinear().range([this.height, 0]);
+        this.z = d3.scaleOrdinal(d3.schemeCategory10);
+
+        this.tooltip = d3.select("#graph-container").append("div").attr("class", "tooltip").style("opacity", 0);
 
         var line = d3.line()
             .curve(d3.curveBasis)
             .x(function (d) { return x(d.date); })
             .y(function (d) { return y(d.score); });
 
+        var selectedSeries = null;
+
+
+        var self = this;
+
         d3.tsv("/Content/datafiles/linechartdata2.txt", type, function (error, data) {
             if (error) throw error;
 
-          
-
-            var seriesNames = d3.keys(data[0])
-                .filter(function (d) { return d !== "date"; })
-                .sort();
-            var series = seriesNames.map(function (series) {
-                return data.map(function (d) {
-                    return { date: d.date, score: d[series] };
-                }).filter(function(v) {
-                    return v.score > 0;
-                });
-            });
-
-            
-            x.domain(d3.extent(d3.merge(series), function (d) { return d.date; })).nice();
-            y.domain(d3.extent(d3.merge(series), function (d) { return d.score; })).nice();
+            self.renderChart(data);
 
 
-          
-            g.append("g")
-                .attr("class", "axis axis--x")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x));
-
-            g.append("g")
-                .attr("class", "axis axis--y")
-                .call(d3.axisLeft(y))
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", "0.71em")
-                .attr("fill", "#000")
-                .text("Score");
-
-
-            g.selectAll(".legend")
-                .data(seriesNames)
-                .enter()
-                
-                .append("text")
-                .attr("y", function(d, i) {
-                    return i * 15;
-                })
-                .style("font", "10px sans-serif")
-                .attr("x",20)
-                .attr("class", "legend")
-                .style("fill", function(d, i) { return z(i) })                
-                
-                .text(function(d) {
-                    return d;
-                });
-
-            
-
-            g.selectAll("dot")
-                .data(series)
-                .enter().append("g")
-                .attr("class", "series")
-                .style("fill", function(d, i) { return z(i) })
-                .selectAll(".point")
-                .data(function(d) { return d; })
-                .enter()
-                .append("circle")
-                .attr("class", "point")
-                .attr("r", function(d) {
-                    if (d.score > 0) {
-                        return 5;
-                    }
-                        return 0;
-
-                    }
-                )
-                .attr("cx", function (d) {
-                    var zz = 42;
-
-                    return x(d.date);
-                })
-                .attr("cy", function (d) {
-                   
-                        return y(d.score);    
-                    
-                  
-                });
-
-             
-
-        
         });
 
         function type(d, _, columns) {
-            d.date = parseTime(d.date);
+            d.date = self.parseTime(d.date);
             for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
             return d;
         }
+
+    }
+
+    GraphD3Controller.prototype.renderChart = function (data) {
+        var self = this; 
+
+        var seriesNames = d3.keys(data[0])
+            .filter(function (d) { return d !== "date"; })
+            .sort();
+        var series = seriesNames.map(function (series) {
+            return data.map(function (d) {
+                return { series: series, date: d.date, score: d[series] };
+            }).filter(function (v) {
+                return v.score > 0;
+            });
+        });
+
+
+        self.x.domain(d3.extent(d3.merge(series), function (d) { return d.date; })).nice();
+        self.y.domain(d3.extent(d3.merge(series), function (d) { return d.score; })).nice();
+
+        self.g.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + self.height + ")")
+            .call(d3.axisBottom(self.x));
+
+        self.g.append("g")
+            .attr("class", "axis axis--y")
+            .call(d3.axisLeft(self.y))
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("fill", "#000")
+            .text("Score");
+
+
+        self.g.selectAll(".legend")
+            .data(seriesNames)
+            .enter()
+
+            .append("text")
+            .attr("y", function (d, i) {
+                return i * 15;
+            })
+            .style("font", "10px sans-serif")
+            .attr("x", 20)
+            .attr("class", "legend")
+            .style("fill", function (d, i) { return self.z(i) })
+
+            .text(function (d) {
+                return d;
+            });
+
+
+
+        self.g.selectAll("dot")
+            .data(series)
+            .enter().append("g")
+            .attr("class", "series")
+            .style("fill", function (d, i) { return self.z(i) })
+            .selectAll(".point")
+            .data(function (d) { return d; })
+            .enter()
+            .append("circle")
+            .style("opacity", 0.5)
+            .attr("class", function (d) {
+                return d.series;
+            })
+            .on("mouseover", function (d) {
+                self.selectedSeries = d.series;
+                self.tooltip.transition().duration(200).style("opacity", .9);
+                self.tooltip.html(d.series + "<br/>" + d.score)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
+                self.tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            })
+            .attr("class", "point")
+            .attr("r", function (d) {
+
+                return 5;
+            })
+            .attr("cx", function (d) {
+                return self.x(d.date);
+            })
+            .attr("cy", function (d) {
+                return self.y(d.score);
+            });
+
 
     }
 

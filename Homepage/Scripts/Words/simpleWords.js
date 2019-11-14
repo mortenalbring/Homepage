b29234428252abc53cd8099fa7ff1868eb78a342
Morten Albring <mortenalbring@gmail.com>
2019-11-14 15:25:27 +0000

    [WordsWithinWords] Multiple svgs

--- Homepage/Scripts/Words/simpleWords.js
+++ Homepage/Scripts/Words/simpleWords.js
@@ -1,37 +1,85 @@
 ﻿function init() {
-    var elemsvg = d3.select("svg"),
-        elemgroup = +svg.attr("group"),
-        elemwidth = +svg.attr("width"),
-        elemheight = +svg.attr("height");
-    
-    
-    
-    
-    function initSvg(svg,group,width,height) {
+    // var elemsvg = d3.select("svg"),
+    //     elemgroup = +svg.attr("group"),
+    //     elemwidth = +svg.attr("width"),
+    //     elemheight = +svg.attr("height");
+    //
+    var svgElems = d3.selectAll(".simpleWords1");
 
-        console.log(group);
+    var svgElem = d3.select("#simpleWords1a");
+
+    initSvg(d3.select("#simpleWords1a"));
+    initSvg(d3.select("#simpleWords1b"));
+
+
+    function initSvg(svg) {
+
+        var group = parseInt(svg.attr("group"));
+        var height = svg.attr("height");
+        var width = svg.attr("width");
 
         var color = d3.scaleOrdinal(d3.schemeCategory20);
-        var forceY = d3.forceY(function(d) {    var fx = d.group * 2;    return fx;}).strength(1);
+        var forceY = d3.forceY(function (d) {
+            var fx = d.group * 2;
+            return fx;
+        }).strength(1);
 
         var simulation = d3.forceSimulation()
-            .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(20).iterations(1))
+            .force("link", d3.forceLink().id(function (d) {
+                return d.id;
+            }).distance(20).iterations(1))
             .force("charge", d3.forceManyBody().distanceMax(40).strength(-40))
             // .force('y', forceY)
             .force("center", d3.forceCenter(width / 2, height / 2));
 
         var radius = 5;
 
-        d3.json("/Scripts/Words/wordSet1.json", function(error, graph) {
+        d3.json("/Scripts/Words/wordSet1.json", function (error, graph) {
             if (error) throw error;
 
+            var filteredNodes = [];
+            for (var i = 0; i < graph.nodes.length; i++) {
+                var n = graph.nodes[i];
+                if (n.group === group) {
+                    
+                    filteredNodes.push(n);
+                }
+            }
+            graph.nodes = filteredNodes;
+            var filteredLinks = [];
+            for (var j = 0; j < graph.links.length; j++) {
+                var searchLink = graph.links[j];
+                var source = searchLink.source;
+                var target = searchLink.target;
+
+                var founds = false;
+                var foundt = false;
+                for (var l = 0; l < graph.nodes.length; l++) {
+                    var searchNode = graph.nodes[l];
+                    if (searchNode.id === source) {
+                        founds = true;
+                    }
+                    if (searchNode.id === target) {
+                        foundt = true;
+                    }
+                }
+                if (founds && foundt) {
+                    filteredLinks.push(searchLink);
+                }
+            }
+            graph.links = filteredLinks;
+
+            console.log(graph.nodes);
+            console.log(graph.links);
 
             var link = svg.append("g")
                 .attr("class", "links")
                 .selectAll("line")
                 .data(graph.links)
                 .enter().append("line")
-                .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+                .attr("stroke-width", function (d) {
+                    return Math.sqrt(d.value);
+                });
 
             var node = svg.append("g")
                 .attr("class", "nodes")
@@ -41,21 +89,25 @@
 
             var circles = node.append("circle")
                 .attr("r", 5)
-                .attr("fill", function(d) { return color(d.group); })
+                .attr("fill", function (d) {
+                    return color(d.group);
+                })
                 .call(d3.drag()
                     .on("start", dragstarted)
                     .on("drag", dragged)
                     .on("end", dragended));
 
             var lables = node.append("text")
-                .text(function(d) {
+                .text(function (d) {
                     return d.id;
                 })
                 .attr('x', 6)
                 .attr('y', 3);
 
             node.append("title")
-                .text(function(d) { return d.id; });
+                .text(function (d) {
+                    return d.id;
+                });
 
             simulation
                 .nodes(graph.nodes)
@@ -66,15 +118,23 @@
 
             function ticked() {
                 link
-                    .attr("x1", function(d) { return d.source.x; })
-                    .attr("y1", function(d) { return d.source.y; })
-                    .attr("x2", function(d) { return d.target.x; })
-                    .attr("y2", function(d) { return d.target.y; });
+                    .attr("x1", function (d) {
+                        return d.source.x;
+                    })
+                    .attr("y1", function (d) {
+                        return d.source.y;
+                    })
+                    .attr("x2", function (d) {
+                        return d.target.x;
+                    })
+                    .attr("y2", function (d) {
+                        return d.target.y;
+                    });
 
                 node
-                    .attr("transform", function(d) {
-                        var resX = Math.max(radius, Math.min(width - radius,d.x));
-                        var resY = Math.max(radius, Math.min(height - radius,d.y));
+                    .attr("transform", function (d) {
+                        var resX = Math.max(radius, Math.min(width - radius, d.x));
+                        var resY = Math.max(radius, Math.min(height - radius, d.y));
                         return "translate(" + resX + "," + resY + ")";
                     })
             }
@@ -97,7 +157,7 @@
             d.fy = null;
         }
     }
-    
+
 }
 
 init();

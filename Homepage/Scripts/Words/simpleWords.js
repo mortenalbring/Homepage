ef00ae2435f147ffbe75f6eebe7332f053bed04a
Morten Albring <mortenalbring@gmail.com>
2019-11-14 15:44:10 +0000

    [WordsWithinWords] Drawing separate

--- Homepage/Scripts/Words/simpleWords.js
+++ Homepage/Scripts/Words/simpleWords.js
@@ -8,28 +8,75 @@
 
     var svgElem = d3.select("#simpleWords1a");
 
-    initSvg(d3.select("#simpleWords1a"));
-    initSvg(d3.select("#simpleWords1b"));
+    initSvg(d3.select("#simpleWords1a"),"simpleWords1p");
+    initSvg(d3.select("#simpleWords1b"),"simpleWords1p");
+    initSvg(d3.select("#simpleWords1c"),"simpleWords1p");
+    initSvg(d3.select("#simpleWords1d"),"simpleWords1p");
 
 
-    function initSvg(svg) {
+    function filterDataOnGroup(graph, group) {
+        var filteredNodes = [];
+        for (var i = 0; i < graph.nodes.length; i++) {
+            var n = graph.nodes[i];
+            if (n.group === group) {
 
+                filteredNodes.push(n);
+            }
+        }
+        graph.nodes = filteredNodes;
+        var filteredLinks = [];
+        for (var j = 0; j < graph.links.length; j++) {
+            var searchLink = graph.links[j];
+            var source = searchLink.source;
+            var target = searchLink.target;
+
+            var founds = false;
+            var foundt = false;
+            for (var l = 0; l < graph.nodes.length; l++) {
+                var searchNode = graph.nodes[l];
+                if (searchNode.id === source) {
+                    founds = true;
+                }
+                if (searchNode.id === target) {
+                    foundt = true;
+                }
+            }
+            if (founds && foundt) {
+                filteredLinks.push(searchLink);
+            }
+        }
+        graph.links = filteredLinks;
+    }
+
+    function initSvg(svg,containerId) {
+        
+        
+        
+        var parentElement = document.getElementById(containerId);
+        
+        var pwidth = parentElement.clientWidth;
+        var pheight = parentElement.clientHeight;
+        
+        console.log(pwidth);
+        
         var group = parseInt(svg.attr("group"));
         var height = svg.attr("height");
         var width = svg.attr("width");
+        console.log(width);
+        console.log(pwidth);
+        
+        width = pwidth;
+        svg.attr("width",width);
 
         var color = d3.scaleOrdinal(d3.schemeCategory20);
-        var forceY = d3.forceY(function (d) {
-            var fx = d.group * 2;
-            return fx;
-        }).strength(1);
+        var forceY = d3.forceY(height / 2).strength(0.85);
 
         var simulation = d3.forceSimulation()
             .force("link", d3.forceLink().id(function (d) {
                 return d.id;
-            }).distance(20).iterations(1))
-            .force("charge", d3.forceManyBody().distanceMax(40).strength(-40))
-            // .force('y', forceY)
+            }).distance(40).iterations(1))
+            .force("charge", d3.forceManyBody().distanceMax(40).strength(-20))
+          //   .force('y', forceY)
             .force("center", d3.forceCenter(width / 2, height / 2));
 
         var radius = 5;
@@ -37,41 +84,8 @@
         d3.json("/Scripts/Words/wordSet1.json", function (error, graph) {
             if (error) throw error;
 
-            var filteredNodes = [];
-            for (var i = 0; i < graph.nodes.length; i++) {
-                var n = graph.nodes[i];
-                if (n.group === group) {
-                    
-                    filteredNodes.push(n);
-                }
-            }
-            graph.nodes = filteredNodes;
-            var filteredLinks = [];
-            for (var j = 0; j < graph.links.length; j++) {
-                var searchLink = graph.links[j];
-                var source = searchLink.source;
-                var target = searchLink.target;
-
-                var founds = false;
-                var foundt = false;
-                for (var l = 0; l < graph.nodes.length; l++) {
-                    var searchNode = graph.nodes[l];
-                    if (searchNode.id === source) {
-                        founds = true;
-                    }
-                    if (searchNode.id === target) {
-                        foundt = true;
-                    }
-                }
-                if (founds && foundt) {
-                    filteredLinks.push(searchLink);
-                }
-            }
-            graph.links = filteredLinks;
-
-            console.log(graph.nodes);
-            console.log(graph.links);
-
+            filterDataOnGroup(graph, group);
+            
             var link = svg.append("g")
                 .attr("class", "links")
                 .selectAll("line")

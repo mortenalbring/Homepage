32ca53a8b3622614377d304566f1254f5ce1c806
Morten Albring <mortenalbring@gmail.com>
2020-11-18 18:17:28 +0000

    [WordsWithinWords] Removed commented code

--- Homepage/Scripts/Words/simpleWords.js
+++ Homepage/Scripts/Words/simpleWords.js
@@ -173,8 +173,6 @@
                         return d.id;
                     }).distance(domVariables.LinkDistance).iterations(1))
                     .force("charge", d3.forceManyBody().strength(domVariables.Charge))
-                    //        .force('y', forceY)
-                    //.force("center", d3.forceCenter(domVariables.Width / 2, domVariables.Height / 2))
                     .force('center', d3.forceCenter().x((domVariables.Width/2)).y((domVariables.Height/2)))
                     .force("attractForce", attractForce)
                     .force("repelForce", repelForce)

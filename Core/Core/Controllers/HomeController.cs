2ff1ddf534649a134a482f9dbdd82d30b581f2c2
Morten Albring <mortenalbring@gmail.com>
2021-06-01 19:36:34 +0100

    Started adding localisation

--- Core/Core/Controllers/HomeController.cs
+++ Core/Core/Controllers/HomeController.cs
@@ -2,24 +2,35 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Core.Models;
+using Microsoft.Extensions.Localization;
 
 namespace Core.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer _localizer;
+        private readonly IStringLocalizer<SharedResource> _sharedLocalizer;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IStringLocalizer<HomeController> localizer,
+            IStringLocalizer<SharedResource> sharedLocalizer, ILogger<HomeController> logger, IStringLocalizerFactory factory)
         {
+            
+            _localizer = localizer;
+            _sharedLocalizer = sharedLocalizer;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            var msg = _sharedLocalizer["test"];
+            var msg2 = _localizer["test"];
+
             return View();
         }
 
--- Core/Core/Startup.cs
+++ Core/Core/Startup.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +28,23 @@ namespace Core
         {
             var builder = services.AddControllersWithViews();
             builder.AddRazorRuntimeCompilation();
+            
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.SubFolder);
+            
+            services.Configure<RequestLocalizationOptions>(
+                opts =>
+                {
+                    var supportedCultures = new[]
+                    {
+                        new CultureInfo("en-GB"),
+                    };
+                    opts.DefaultRequestCulture = new RequestCulture("nb-NO");
+                    // Formatting numbers, dates, etc.
+                    opts.SupportedCultures = supportedCultures;
+                    // UI strings that we have localized.
+                    opts.SupportedUICultures = supportedCultures;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

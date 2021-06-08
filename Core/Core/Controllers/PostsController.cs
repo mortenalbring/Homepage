86e31fd666b10083c61a69af24a34e932eabcb87
Morten Albring <mortenalbring@gmail.com>
2021-06-08 14:18:48 +0100

    Added Localisation post

---
+++ Core/Core/Controllers/PostsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly IStringLocalizer _localizer;
+        private readonly IStringLocalizer<SharedResource> _sharedLocalizer;
+
+        
+        public PostsController(IStringLocalizer<PostsController> localizer,
+            IStringLocalizer<SharedResource> sharedLocalizer, ILogger<PostsController> logger)
+        {
+            _localizer = localizer;
+            _sharedLocalizer = sharedLocalizer;
+        }
+
+        public IActionResult Localisation()
+        {
+            return View();
+        }
+    }
+}

dd8e0315b40c2c95d15a967edf7aa3c69f2b5e50
Morten Albring <mortenalbring@gmail.com>
2021-06-24 17:29:54 +0100

    Fiddled with app

--- Core/Core/wwwroot/js/fractalChaos/fractalChaos.js
+++ Core/Core/wwwroot/js/fractalChaos/fractalChaos.js
@@ -6,42 +6,64 @@ var height = 400;
 var nodes = [];
 var x = 400;
 var y = 400;
-var iterations = 100;
-var radius = 2;
+var iterations = 1000;
+var radius = 0.5;
 
 var marginLeft = 10;
-var marginTop = 10;
+var marginTop = 1;
 
-var anchorRadius = 4;
+var anchorRadius = 2;
+var animSpeed = 10;
 
 document.getElementById('iterMaxCount').value = iterations;
+document.getElementById('dataRadius').value = radius;
+document.getElementById('dataSpeed').value = animSpeed;
 
 var svg = d3.select("#fractal").append("svg")
-    .attr("width", width)
-    .attr("height", height)
+//    .attr("width", width)
+//    .attr("height", height)
     .attr("style","border: 1px solid white")
-//    .attr("viewBox","0 0 100 100")
+    .attr("viewBox","0 0 100 100")
 ;
 
-
-
-//restart();
 function drawAnchors() {
     calc();
     drawUpTo(3);
 }
-drawAnchors();
-function calc() {
-    
+
+
+function parseGuiVariables() {
     var iterTextVal = document.getElementById('iterMaxCount').value;
     var parsedInt = parseInt(iterTextVal,10);
     if (parsedInt > 0) {
         iterations = parsedInt;
     }
+
+    var radiusTextVal= document.getElementById('dataRadius').value;
+    var parsedRadius = parseFloat(radiusTextVal);
+    if (parsedRadius > 0) {
+        radius = parsedRadius;
+    }
+
+    var speedTextVal = document.getElementById('dataSpeed').value;
+    var parsedSpeed = parseFloat(speedTextVal);
+    if (parsedSpeed > 0) {
+        animSpeed = parsedSpeed;
+    }
+}
+
+function calc() {
+    parseGuiVariables();
     
-    nodes[0] = {x: width - anchorRadius, y: height - anchorRadius, r: 255, g: 0, b: 0, radius: anchorRadius};
-    nodes[1] = {x: anchorRadius, y: height - anchorRadius, r: 0, g: 255, b: 0, radius: anchorRadius};
-    nodes[2] = {x: (width / 2), y: anchorRadius, r: 0, g: 0, b: 255, radius: anchorRadius};
+    
+    var strokeWidthAnchor = 1.0;
+    var strokeWidthPoint = radius / 10;
+    
+    var anchorOffset = 5;
+    
+    nodes[0] = {x: width - anchorRadius-anchorOffset, y: height - anchorRadius-anchorOffset, r: 255, g: 0, b: 0, radius: anchorRadius, strokeWidth: strokeWidthAnchor};
+    nodes[1] = {x: anchorRadius+anchorOffset, y: height - anchorRadius-anchorOffset, r: 0, g: 255, b: 0, radius: anchorRadius, strokeWidth: strokeWidthAnchor};
+    nodes[2] = {x: (width / 2), y: anchorRadius+anchorOffset, r: 0, g: 0, b: 255, radius: anchorRadius, strokeWidth: strokeWidthAnchor};
 
     var r = 0;
     var g = 0;
@@ -71,7 +93,7 @@ function calc() {
                 b = 255;
                 break
         }
-        nodes[i] = {x: x, y: y, r: r, g: g, b: b, radius: radius}
+        nodes[i] = {x: x, y: y, r: r, g: g, b: b, radius: radius, strokeWidth: strokeWidthPoint}
     }
     nodes.length = iterations;
 }
@@ -85,9 +107,12 @@ function stop() {
 }
 
 
-function drawUpTo(max) {
-    if (max >= nodes.length) {
-        stop();
+function drawUpTo(max, stopOnMax) {
+    if (max > nodes.length) {
+        if (stopOnMax) {
+            stop();
+        }
+        return;
     }
     var subsetNodes = [];
     document.getElementById('iter').textContent = max + "/" + nodes.length;
@@ -100,13 +125,21 @@ function drawUpTo(max) {
     node.enter().insert("circle")
 
     node.attr("cx", function (d) {
-        return d.x
+        var xperc = (d.x / width) * 100 + "%";
+        return xperc
     })
         .attr("cy", function (d) {
-            return d.y
+            var yperc = (d.y / height) * 100 + "%";
+            return yperc
         })
         .attr("r", function (d) {
             return d.radius
+        })
+        .style("stroke-width", function(d) {
+            return d.strokeWidth;
+        })
+        .style("stroke", function (d) {
+            return "white";
         })
         .style("fill", function (d) {
             return "rgb(" + d.r + "," + d.g + "," + d.b + ")"
@@ -120,14 +153,33 @@ function drawUpTo(max) {
 function restart() {
     stop();
     calc();
- 
-
+    
     var i = 1;
     setInterval(function () {
         if (i < iterations) {
             i++;
         }
-        drawUpTo(i);
-    }, 10);
+        drawUpTo(i,true);
+    }, animSpeed);
 
 }
+
+
+function startLoop() {
+    stop();
+    calc();
+
+    var i = 1;
+    setInterval(function () {
+        if (i <= iterations) {
+            i++;
+        }
+        else {
+            i = 1;
+        }
+        drawUpTo(i, false);
+    }, animSpeed);
+
+}
+drawAnchors();
+startLoop();

7a349350a6c943963781f3bc95aa06ff38d3bde7
Morten Albring <mortenalbring@gmail.com>
2021-08-23 16:57:35 +0100

    Added words bar data

--- Core/Core/wwwroot/words/barChart.js
+++ Core/Core/wwwroot/words/barChart.js
@@ -11,30 +11,43 @@ var g = svg.append("g")
     .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
 d3.csv("/words/bar-data.csv").then(function(flatData) {
-    xScale.domain(flatData.map(function(d) { return d.year; }));
-    yScale.domain([0, d3.max(flatData, function(d) { return d.value; })]);
+    console.log(flatData);
+    xScale.domain(flatData.map(function(d) { return d.wordlength; }));
+    yScale.domain([0, d3.max(flatData, function(d) { return parseInt(d.words); })]);
 
+    var maxval = d3.max(flatData, function(d) { return parseInt(d.words); });
+    console.log(maxval);
+    
     g.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xScale));
+        .call(d3.axisBottom(xScale))
+        .append("text")
+        .attr("y", 20)
+        .attr("x", (width/2))
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "middle")
+        .text("Length of word");
+    
 
     g.append("g")
         .call(d3.axisLeft(yScale).tickFormat(function(d){
-            return "$" + d;
+            return d;
         }).ticks(10))
         .append("text")
         .attr("y", 6)
+        .attr("x", 6)
         .attr("dy", "0.71em")
         .attr("text-anchor", "end")
-        .text("value");
+        .text("Number of words");
 
     g.selectAll(".bar")
         .data(flatData)
         .enter().append("rect")
         .attr("class", "bar")
-        .attr("x", function(d) { return xScale(d.year); })
-        .attr("y", function(d) { return yScale(d.value); })
+        .attr("fill","orange")
+        .attr("x", function(d) { return xScale(d.wordlength); })
+        .attr("y", function(d) { return yScale(parseInt(d.words)); })
         .attr("width", xScale.bandwidth())
-        .attr("height", function(d) { return height - yScale(d.value); });
+        .attr("height", function(d) { return height - yScale(parseInt(d.words)); });
     
 });

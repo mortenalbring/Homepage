5fa73d3b2df1185cbf155f95fa3b93f7aa356933
Morten Albring <mortenalbring@gmail.com>
2021-09-01 12:55:35 +0100

    Reloading data

--- Core/Core/wwwroot/words/searchWordsv2.js
+++ Core/Core/wwwroot/words/searchWordsv2.js
@@ -1,97 +1,130 @@
 ﻿
-d3.timeout(function() {
+
+
+
+function drawGraph(graphData) {
 
     var svg = d3.select("#simpleWords2wd"),
         width = +svg.attr("width"),
         height = +svg.attr("height");
 
-    var color = d3.scaleOrdinal();
+
+    var maxval = Math.max.apply(Math, graphData.nodes.map(function(o) { return o.group; }))
+    var minval = Math.min.apply(Math, graphData.nodes.map(function(o) { return o.group; }))
+
+
+    var color = d3.scaleLinear()
+        .domain([minval, maxval])
+        .range(["red", "blue", "green"]);
+    
 
     var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d) { return d.id; }))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2));
+        .force("link", d3.forceLink().id(function (d) {
+            return d.id;
+        }))
+        .force("charge", d3.forceManyBody().strength(-5))
+        .force("center", d3.forceCenter(width / 2, height / 2).strength(1));
 
-    console.log(svg);
-    var path = "/words/Json/" + "simpleWordDataEnglish.json";
-
-
-    d3.json(path).then(function(graph) {
-    console.log(graph);
 
     var link = svg.append("g")
         .attr("class", "links")
         .selectAll("line")
-        .data(graph.links)
+        .data(graphData.links)
         .enter().append("line")
-        .attr("stroke-width", function(d) { return 1 });
+        .attr("stroke-width", function (d) {
+            return 1
+        });
 
     var node = svg.append("g")
         .attr("class", "nodes")
         .selectAll("g")
-        .data(graph.nodes)
+        .data(graphData.nodes)
         .enter().append("g")
 
+    var radius = 5;
+
     var circles = node.append("circle")
-        .attr("r", 5)
-        .attr("fill", function(d) { return color(d.group); });
+        .attr("r", radius)
+        .attr("fill", function (d) {
+            return color(d.group);
+        });
 
-
-        const D = d3.drag()
-            .on("start", (event, d) => {})
-            .on("drag", (event, d) => {
-                //d.x = event.x;
-                //d.y = event.y;
-            })
-            .on("end", (event, d) => {});
-        node.call(D);
-
-        var lables = node.append("text")
-        .text(function(d) {
+    var lables = node.append("text")
+        .text(function (d) {
             return d.id;
         })
         .attr('x', 6)
         .attr('y', 3);
 
     node.append("title")
-        .text(function(d) { return d.id; });
+        .text(function (d) {
+            return d.id;
+        });
+
 
     simulation
-        .nodes(graph.nodes)
+        .nodes(graphData.nodes)
         .on("tick", ticked);
 
     simulation.force("link")
-        .links(graph.links);
+        .links(graphData.links);
 
     function ticked() {
         link
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+            .attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
 
         node
-            .attr("transform", function(d) {
-                return "translate(" + d.x + "," + d.y + ")";
+            .attr("transform", function (d) {
+                var newx = Math.max(radius, Math.min(width - radius, d.x));
+                var newy = Math.max(radius, Math.min(width - radius, d.y));
+                return "translate(" + newx + "," + newy + ")";
             })
     }
-});
 
-function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
+
 }
 
-function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-}
+$('#btnSearchWord').click(function() {
+    var svg = d3.select("#simpleWords2wd");
+    
+    svg.selectAll("*").remove();
 
-function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-}
+    var newterm = "test";
+    var path = "/words/Json/" + "EnglishSowpodsdeepwordchain.json";
+
+    d3.json(path).then(function (graph) {
+        console.log(graph);
+
+        var graphData = WordsGeneral.FilterDataOnTerm(graph, newterm);
+
+        drawGraph(graphData);
+
+    });
+})
+
+d3.timeout(function () {
+    
+    var path = "/words/Json/" + "EnglishSowpodsdeepwordchain.json";
+    
+    d3.json(path).then(function (graph) {
+        console.log(graph);
+
+        var graphData = WordsGeneral.FilterDataOnTerm(graph, "mort");
+
+      drawGraph(graphData);
+      
+    });
+
 
 });

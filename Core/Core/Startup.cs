f48d717cf381d030a8f3557c665f916dd126d2f1
Morten Albring <mortenalbring@gmail.com>
2021-10-05 17:03:05 +0100

    Forcing https redirect

--- Core/Core/Startup.cs
+++ Core/Core/Startup.cs
@@ -80,7 +80,7 @@ namespace Core
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseHttpsRedirection();
             app.UseWebOptimizer();
             
             app.UseRequestLocalization();

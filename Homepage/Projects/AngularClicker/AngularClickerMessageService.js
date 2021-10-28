6e0214c8e6f46cedc8e9d64df5994bf51e5ae759
Morten Albring <mortenalbring@gmail.com>
2021-10-28 12:35:50 +0100

    Removed old homepage

--- Homepage/Projects/AngularClicker/AngularClickerMessageService.js
+++
@@ -1,7 +0,0 @@
-var AngularClickerMessageService = function () {
-    /// <summary>
-    /// This service just keeps a log of the messages printed to the screen
-    /// </summary>
-    this.MessageLog = [];
-
-}

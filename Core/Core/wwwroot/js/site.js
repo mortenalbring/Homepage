aa5f634e3d26c6ffd0919293ef6f44a9623927a8
Morten Albring <mortenalbring@gmail.com>
2021-11-30 13:29:43 +0000

    Added date parsing

--- Core/Core/wwwroot/js/site.js
+++ Core/Core/wwwroot/js/site.js
@@ -1,2 +1,13 @@
-﻿var now = moment();
-console.log(now);
+﻿var dv = $('#dateval');
+if (dv) {
+    var textVal = dv.text();
+    console.log(textVal);
+    var mom = moment(textVal);
+    console.log(mom);
+    console.log(mom.fromNow());
+    var fromNowText = "(" + mom.fromNow() + ")";
+    var niceText = mom.format("dddd [the] Do MMMM YYYY ")
+    $('#datevalago').text(fromNowText);
+    $('#datevalnice').text(niceText);
+    $('#dateval').hide();
+}

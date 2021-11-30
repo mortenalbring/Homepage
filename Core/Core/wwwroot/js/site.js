1697a2aa41d3ec3545febceba64a2e06acdf8afa
Morten Albring <mortenalbring@gmail.com>
2021-11-30 13:34:09 +0000

    Added styling and null check

--- Core/Core/wwwroot/js/site.js
+++ Core/Core/wwwroot/js/site.js
@@ -1,13 +1,13 @@
-﻿var dv = $('#dateval');
-if (dv) {
-    var textVal = dv.text();
-    console.log(textVal);
-    var mom = moment(textVal);
-    console.log(mom);
-    console.log(mom.fromNow());
-    var fromNowText = "(" + mom.fromNow() + ")";
-    var niceText = mom.format("dddd [the] Do MMMM YYYY ")
-    $('#datevalago').text(fromNowText);
-    $('#datevalnice').text(niceText);
-    $('#dateval').hide();
+﻿if (document.getElementById("dateval")) {
+    var dv = $('#dateval');
+    if (dv) {
+        var textVal = dv.text();
+        var mom = moment(textVal);
+        console.log(mom.fromNow());
+        var fromNowText = "(" + mom.fromNow() + ")";
+        var niceText = mom.format("dddd [the] Do MMMM YYYY ")
+        $('#datevalago').text(fromNowText);
+        $('#datevalnice').text(niceText);
+        $('#dateval').hide();
+    }    
 }

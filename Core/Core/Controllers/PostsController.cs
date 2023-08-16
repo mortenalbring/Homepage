80828676a115cfd315a7bd2215f844cd0f9127d1
Morten Albring <morten.albring@smartr365.com>
2023-08-16 09:52:04 +0100

    Removed unused refs

--- Core/Core/Controllers/PostsController.cs
+++ Core/Core/Controllers/PostsController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using Core.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 
